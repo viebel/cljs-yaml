43db58993ab78350b4de05a0d91deb54a008589b
Yehonathan Sharvit <viebel@gmail.com>
2018-07-19 10:28:07 +0300

    version 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject viebel/cljs-yaml "0.0.1"
+(defproject viebel/cljs-yaml "0.0.2"
   :description "js-yaml wrapper for clojurescript"
   :url "https://github.com/viebel/cljs-yaml"
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
