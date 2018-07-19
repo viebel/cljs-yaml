c023f16666632e921af65753e394c2ff1de1daf1
Yehonathan Sharvit <viebel@gmail.com>
2018-07-19 10:06:59 +0300

    configure lein for clojars

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject viebel/cljs-yaml "0.0.1"
+  :description "js-yaml wrapper for clojurescript"
+  :url "https://github.com/viebel/cljs-yaml"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/clojurescript "1.9.227"]]
+  :source-paths ["src/cljs"]
+  :deploy-repositories {"clojars" {:url "https://repo.clojars.org"
+                                   :sign-releases false}})
